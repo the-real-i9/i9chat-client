dde7c71ea9f36ed68177e78d07f13582cc487177
the-real-i9 <oluwarinolasam@gmail.com>
2025-07-02 12:57:33 +0100

    integrating capacitor for mobile support

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+import reactLogo from '/logos/react.svg'
+import viteLogo from '/logos/vite.svg'
+import { Toast } from '@capacitor/toast';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const showHelloToast = async (count) => {
+    await Toast.show({
+      text: 'Hello! ' + count,
+      position: 'top'
+    });
+  };
+
+  
+  function handleClick() {
+    setCount((count) => count + 1)
+
+    showHelloToast(count + 1)
+  }
 
   return (
     <>
       <div className="flex items-center justify-center">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo h-[6em] p-[1.5em] will-change-[filter] transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
+          <img src={viteLogo} className="logo h-[6em] p-[1.5em] will-change-[filter] transition-[filter] duration-300 drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react h-[6em] p-[1.5em] will-change-[filter] transition-[filter] duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-logo-spin" alt="React logo" />
+          <img src={reactLogo} className="logo react h-[6em] p-[1.5em] will-change-[filter] transition-[filter] duration-300 drop-shadow-[0_0_2em_#61dafbaa] animate-logo-spin" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-bold'>i9chat</h1>
       <div className="card p-[2em]">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='outline-none' onClick={handleClick}>
           count is {count}
         </button>
         <p>
