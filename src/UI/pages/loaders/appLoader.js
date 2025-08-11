5afd577946bef67e69557d5ea81636de5cb754dd
the-real-i9 <oluwarinolasam@gmail.com>
2025-08-11 18:34:54 +0100

    user session request

--- src/UI/pages/loaders/appLoader.js
+++ src/UI/pages/loaders/appLoader.js
@@ -1,20 +1,27 @@
-import { redirect } from "react-router";
-import store from "../../../store";
-import { setUser } from "../../../store/userSlice";
+import { redirect } from "react-router"
+import store from "../../../store"
+import { setUser } from "../../../store/userSlice"
+import axios from "axios"
 
 export default async function appLoader() {
+  try {
+    const resp = await axios.get(
+      "http://localhost:8000/api/app/user/session_user",
+      { withCredentials: true }
+    )
+    console.log(resp)
 
-  // simulate API request
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+    const userInfo = {
+      id: "1",
+      username: "i9ine",
+      profilePicUrl: "",
+    }
 
-  const userInfo = {
-    id: "1",
-    username: "i9ine",
-    profilePicUrl: "",
+    store.dispatch(setUser(userInfo))
 
+    return null
+  } catch (error) {
+    if (error.status == 401) return redirect("/login")
+    else console.error(error)
   }
-
-  store.dispatch(setUser(userInfo))
-
-  return redirect("/login")
 }
